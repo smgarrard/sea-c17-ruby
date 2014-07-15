d9398a00c600e970b77c6f5c8a20e22f0f9b0ff2
Ryan Sobol <contact@ryansobol.com>
2014-07-15 14:03:46 -0700

    Cosmetic suggestions

--- class2/8_table_of_contents.rb
+++ class2/8_table_of_contents.rb
@@ -14,8 +14,8 @@ full_width = 50
 left_width = 30
 right_width = 20
 
-puts('Table of Contents'.center(full_width))
+puts 'Table of Contents'.center(full_width)
 puts
-puts ('Chapter 1:  Getting Started'.ljust(left_width) + 'page  1'.rjust(right_width))
-puts ('Chapter 2:  Numbers'.ljust(left_width) + 'page  9'.rjust(right_width))
-puts ('Chapter 3:  Letters'.ljust(left_width) + 'page 13'.rjust(right_width))
+puts 'Chapter 1:  Getting Started'.ljust(left_width) + 'page  1'.rjust(right_width)
+puts 'Chapter 2:  Numbers'.ljust(left_width) + 'page  9'.rjust(right_width)
+puts 'Chapter 3:  Letters'.ljust(left_width) + 'page 13'.rjust(right_width)
